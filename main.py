c9ae4d8981695d395c85e7198edda7bc2dc4f06d
karthickeyan17 <skarthickeyan707@gmail.com>
2024-07-14 12:16:44 +0530

    corrected logical error of updating existing  date's data

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd 
 import scraper as Scraper
 from datetime import datetime ,timedelta
+import os
 
 DB_PATH= "./Student_db.csv"
 OUTPUT_PATH = "./leetcode_stats.csv"
 def fetch_stats(date):
     df = pd.read_csv(DB_PATH)
-    res=pd.read_csv(OUTPUT_PATH,header=[0, 1],index_col=[0])
     df2=None
     for index, row in df.iterrows():
         problems_byDate = Scraper.getProblems(row['Leetcode ID'])
@@ -20,13 +20,15 @@ def fetch_stats(date):
             df2=pivot_df
         else:
             df2=pd.concat([df2,pivot_df],axis=0)
-
-    res = pd.merge(df2, res, left_index=True, right_index=True)
-    # res.to_csv(OUTPUT_PATH)
-    print(res)   
+    if os.path.isfile(OUTPUT_PATH):
+        df1=pd.read_csv(OUTPUT_PATH,header=[0, 1],index_col=[0])
+        df1.update(df2)
+        df1.to_csv(OUTPUT_PATH)
+    else:
+        df2.to_csv(OUTPUT_PATH)  
+    print(pd.read_csv(OUTPUT_PATH))
     
 if __name__ == "__main__":
-
     date = datetime.now().date()
     print("Fetching for date:",date)
     fetch_stats(date)
