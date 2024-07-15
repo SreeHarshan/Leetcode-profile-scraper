7eace4b1f547d15cc856d391d8d72f650b6e55f6
karthickeyan17 <skarthickeyan707@gmail.com>
2024-07-15 22:03:40 +0530

    update

--- cache.py
+++ cache.py
@@ -1,28 +1,21 @@
-
 cache = {}
 CACHE_PATH = "./cache.txt"
 
 def initIfNot():
-    if(len(cache)!=0):
+    if len(cache) != 0:
         return
-    f = open(CACHE_PATH,"r")
+    f = open(CACHE_PATH, "r")
     for line in f.readlines():
-        p,d = line.split(",")
-        cache[p] = d
+        p, d = line.split(",")
+        cache[p] = d.strip()
     f.close()
 
-    # print("Cache:")
-    # print(cache)
-    # print("\n\n")
-
-
-def addCache(problem,difficulty):
-    f = open(CACHE_PATH,"a")
-    f.write(problem+","+difficulty+"\n")
+def addCache(problem, difficulty):
+    f = open(CACHE_PATH, "a")
+    f.write(problem + "," + difficulty + "\n")
     f.close()
-
     cache[problem] = difficulty
-    print("added cache:",problem,difficulty)
+    print("added cache:", problem, difficulty)
 
 def getCache(problem):
     return cache[problem]
